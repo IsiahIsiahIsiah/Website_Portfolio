3249bb9356647786c2a3a7f086b9427df7d9b0a8
Isiah Toilalo <20773146@student.westernsydney.edu.au>
2025-06-05 02:03:28 +1000

    added navbar.php. changed index to php.

---
+++ navBar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Navbar</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="navbarLeft">
+
+        </div>
+        <div class="navbarCentre">
+            <div class="navbarButton">
+                <a href="index.php">Home</a>
+                <a href="aboutme.php">About Me</a>
+                <a href="contactme.php">Contact Me</a>
+            </div>
+        </div>
+        <div class="navbarRight">
+
+        </div>
+    </body>
+</html>
